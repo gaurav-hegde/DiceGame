a26e791572536b67dce78d7d9c2e687bc77cc323
Gaurav Hegde <gaurav.hegde2021@vitstudent.ac.in>
2022-03-21 14:06:13 +0530

    Comments

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+//First dice
 var randomNumber1 = Math.floor(Math.random()*6)+1; //random number between 1-6
-
 var diceNumber = "images/dice" + randomNumber1 + ".png"; //sets a source to images randomly
 
 var dice1 = document.querySelector(".img1");
@@ -12,6 +12,7 @@ var diceNumber2 = "images/dice" + randomNumber2 + ".png";
 var dice2 = document.querySelector(".img2");
 dice2.setAttribute("src", diceNumber2);
 
+//Changing the heading
 if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "Player 1 wins!";
 } else {
